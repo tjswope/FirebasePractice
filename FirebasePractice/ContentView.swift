c3299686b1ae26216808b337cdbe26890031e45f
Tom Swope <tjswope@gmail.com>
2026-03-09 09:33:47 -0400

    Initial Commit

---
+++ FirebasePractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebasePractice
+//
+//  Created by Swope, Thomas on 3/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebasePractice/FirebasePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePracticeApp.swift
+//  FirebasePractice
+//
+//  Created by Swope, Thomas on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
